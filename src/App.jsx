f746668909d165856d52c040a6f563c7ca8d186e
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2024-02-05 16:20:35 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 
-const App = () => {
-  return <div>App</div>;
-};
+import Events from './components/Events/Events.jsx';
+import EventDetails from './components/Events/EventDetails.jsx';
+import NewEvent from './components/Events/NewEvent.jsx';
+import EditEvent from './components/Events/EditEvent.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/events" />,
+  },
+  {
+    path: '/events',
+    element: <Events />,
+
+    children: [
+      {
+        path: '/events/new',
+        element: <NewEvent />,
+      },
+    ],
+  },
+  {
+    path: '/events/:id',
+    element: <EventDetails />,
+    children: [
+      {
+        path: '/events/:id/edit',
+        element: <EditEvent />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
